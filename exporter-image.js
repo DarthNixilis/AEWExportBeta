c3f423bdf7b8f78dc99fcb98a68c1b36ec1e81ea
DarthNixilis <65317550+DarthNixilis@users.noreply.github.com>
2026-01-07 20:54:12 -0700

    Create exporter-image.js

---
+++ exporter-image.js
@@ -0,0 +1,6 @@
+// exporter-image.js
+// Implement later. For now, it exists so imports don't crash the app.
+
+export function exportDeckAsImage() {
+  throw new Error("exportDeckAsImage() not implemented yet (exporter-image.js).");
+}
