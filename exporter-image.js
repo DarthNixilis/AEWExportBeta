472f3e3b177f69aa72e08f48fa51624c5cae10a2
DarthNixilis <65317550+DarthNixilis@users.noreply.github.com>
2026-01-10 00:30:49 -0700

    Delete exporter-image.js

--- exporter-image.js
+++
@@ -1,6 +0,0 @@
-// exporter-image.js
-// Implement later. For now, it exists so imports don't crash the app.
-
-export function exportDeckAsImage() {
-  throw new Error("exportDeckAsImage() not implemented yet (exporter-image.js).");
-}
